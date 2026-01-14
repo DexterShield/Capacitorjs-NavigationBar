30e8438e1352d61a536e248b1ff28894dcbbe7e1
YosriGFX <yosri@dextershield.com>
2026-01-14 01:12:25 +0100

    Initial commit

---
+++ android/src/main/java/com/dextershield/plugins/navigation_bar/NavigationBar.java
@@ -0,0 +1,11 @@
+package com.dextershield.plugins.navigation_bar;
+
+import com.getcapacitor.Logger;
+
+public class NavigationBar {
+
+    public String echo(String value) {
+        Logger.info("Echo", value);
+        return value;
+    }
+}
